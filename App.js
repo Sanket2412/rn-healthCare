2f305088ed021699b1d4125fff7331d8191469c1
Sanket Khardekar <khardekarsanket@gmail.com>
2022-02-23 23:20:52 +0530

    Update App.js
    Updated App.js File

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './screens/HomeScreen';
-
+import { View } from "react-native";
+import HomeScreen from "./screens/HomeScreen";
 export default function App() {
   return (
     <View>

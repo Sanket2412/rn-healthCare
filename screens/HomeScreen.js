5e8851c5cebbeb9aad3a7aac349c2cf98b12c7fa
Sanket Khardekar <khardekarsanket@gmail.com>
2022-06-04 11:48:14 +0530

    Add VisitsList File

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,31 @@
 import React, {useLayoutEffect} from "react";
-import { View, StyleSheet, ScrollView, Dimensions } from "react-native";
+import { View, StyleSheet, Dimensions, Linking,ImageBackground } from "react-native";
 import { Button } from "react-native-paper";
-import EmergencyDetails from "../component/Patient/HomePage/EmergencyDetails";
+import { Fragment } from "react";
+import VisitsList from "../component/Patient/HomePage/VisitsList";
 
-const { height, width } = Dimensions.get("window");
+const { height } = Dimensions.get("window");
+const Doctor = {
+  name: "🧑‍⚕️Dr. Shah",
+  phoneNumber: 8087309626,
+  clinicAddress: "🏥Demo Address Near Demo Road!",
+};
 const HomeScreen = (props) => {
+  const dialCall = () => {
+    let phoneNumber = "";
+    if (Platform.OS === "android") {
+      phoneNumber = `tel:${Doctor.phoneNumber}`;
+    } else {
+      phoneNumber = `telprompt:${Doctor.phoneNumber}`;
+    }
+
+    Linking.openURL(phoneNumber);
+  };
   useLayoutEffect(() => {
     props.navigation.setOptions({
       headerRight: () => (
+        <Fragment>
+          <View style={{flexDirection:"row",}}>
         <Button
           icon="account-box"
           labelStyle={{ fontSize: 27 }}
@@ -15,26 +33,31 @@ const HomeScreen = (props) => {
           title=""
           color="white"
         />
+        <Button
+          icon="phone"
+          labelStyle={{ fontSize: 27 }}
+          onPress={dialCall}
+          title=""
+          color="white"
+        /></View></Fragment>
       ),
     });
   }, [props.navigation]);
   return (
-    <ScrollView style={styles.app}>
-      <View style={styles.container}>
-        <EmergencyDetails />
+    <ImageBackground source={{uri:"https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX26510670.jpg"}} style={{flexDirection:"column",height:"100%"}}>
+      <View style={styles.app}>
+      <VisitsList />
       </View>
-    </ScrollView>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
   app: {
-    flex:1,
-    backgroundColor: "#ffedff",
-    //backgroundColor: "#039dfc",
-  },
-  container: {
-    marginTop: height / 11,
+    height:(height/ 2),
+    paddingTop:(height * 0.1),
+    flexDirection:"column",
+    alignItems:"center"
   },
 });
 export default HomeScreen;

8c6a8db94c19efdebdd791f016726827f6440295
Sanket Khardekar <khardekarsanket@gmail.com>
2022-06-04 11:42:34 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 import Tabs from "./navigation/tabsBottomNavigation";
 import { NavigationContainer } from "@react-navigation/native";
 export default function App() {

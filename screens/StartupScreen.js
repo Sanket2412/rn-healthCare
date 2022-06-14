f11922311cc936d1e5f92e78cd58637fdf32eae9
Sanket Khardekar <khardekarsanket@gmail.com>
2022-06-15 00:08:42 +0530

    Create StartupScreen.js

---
+++ screens/StartupScreen.js
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import * as authActions from "../store/actions/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { View , StyleSheet, ActivityIndicator} from "react-native";
+const StartupScreen=()=>{
+    const dispatch=useDispatch();
+    useEffect(()=>{
+        const tryLogin=async()=>{
+            const userData=await AsyncStorage.getItem('userData');
+            if(!userData)
+            {
+                dispatch(authActions.setDidTryAL());
+                return;
+            }
+            const transformedData=JSON.parse(userData);
+            const { token, userId, expiryDate }=transformedData;
+            const expirationDate=new Date(expiryDate);
+            if(expirationDate<= new Date() || !token || !userId)
+            {
+                dispatch(authActions.setDidTryAL());
+                return;
+            }
+            const expirationTime=expirationDate.getTime() - new Date().getTime();
+            dispatch(authActions.authenticate(userId,token,expirationTime))
+        }
+        tryLogin();
+    },[dispatch])
+    return <View style={styles.screen}>
+        <ActivityIndicator size="large" color="blue" />
+    </View>
+}
+
+const styles= StyleSheet.create({
+    screen:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+    }
+})
+
+export default StartupScreen;

9446fb6e59dd2906aadd3da522be954ec8e19628
Sanket Khardekar <khardekarsanket@gmail.com>
2022-06-15 00:06:22 +0530

    Added Redux and Redux thunk.

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React from "react";
-import Tabs from "./navigation/tabsBottomNavigation";
-import { NavigationContainer } from "@react-navigation/native";
+import AppNavigator from "./navigation/AppNavigator";
+import ReduxThunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { applyMiddleware, combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./store/reducers/auth";
+const rootReducer=combineReducers({
+  auth:authReducer
+});
+const store=configureStore({reducer:rootReducer},applyMiddleware(ReduxThunk))
 export default function App() {
   return (
-      <NavigationContainer >
-        <Tabs />
-      </NavigationContainer>
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
   );
 }

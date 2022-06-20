2e50addb9b83298d367d1ad14b09b603ba109a8d
Sanket Khardekar <khardekarsanket@gmail.com>
2022-06-20 10:05:16 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,1 +1,12 @@
 export const defaultApiUrl="https://rn-healthcare-default-rtdb.firebaseio.com/";
+export const API_KEY="AIzaSyBDcwnejdj6BW0lO0EYdEuq94Zp-dUQyM4";
+export const firebaseConfig = {
+    apiKey: "AIzaSyBDcwnejdj6BW0lO0EYdEuq94Zp-dUQyM4",
+    authDomain: "rn-healthcare.firebaseapp.com",
+    databaseURL: "https://rn-healthcare-default-rtdb.firebaseio.com",
+    projectId: "rn-healthcare",
+    storageBucket: "rn-healthcare.appspot.com",
+    messagingSenderId: "444229026835",
+    appId: "1:444229026835:web:2dc87e16a4d988c02f2be6",
+    measurementId: "G-NGTZZYNJ3F"
+  };

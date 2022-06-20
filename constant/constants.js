17e3a55a7ebbc81b8d0683f2042e37b18f569da5
Sanket Khardekar <khardekarsanket@gmail.com>
2022-06-20 10:05:27 +0530

    Update constants.js

--- constant/constants.js
+++ constant/constants.js
@@ -1,1 +1,36 @@
-export const background="https://png.pngtree.com/thumb_back/fh260/background/20190222/ourmid/pngtree-blue-hospital-medical-western-medicine-poster-background-backgroundmedical-backgrounddoctor-backgroundhospital-image_63770.jpg";
+export const defaultAvatar ="https://as1.ftcdn.net/v2/jpg/03/53/11/00/1000_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg";
+export const background ="https://png.pngtree.com/thumb_back/fh260/background/20190222/ourmid/pngtree-blue-hospital-medical-western-medicine-poster-background-backgroundmedical-backgrounddoctor-backgroundhospital-image_63770.jpg";
+export const bloodGroupData = [
+  {
+    value: "A+",
+    label: "A+",
+  },
+  {
+    value: "A-",
+    label: "A-", 
+  },
+  {
+    value: "B+",
+    label: "B+",
+  },
+  {
+    value: "B-",
+    label: "B-",
+  },
+  {
+    value: "O+",
+    label: "O+",
+  },
+  {
+    value: "O-",
+    label: "O-",
+  },
+  {
+    value: "AB+",
+    label: "AB+",
+  },
+  {
+    value: "AB-",
+    label: "AB-",
+  },
+];

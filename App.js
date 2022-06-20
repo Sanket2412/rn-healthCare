7107e0ca943617dded480acd6688831bf16144c1
Sanket Khardekar <khardekarsanket@gmail.com>
2022-06-20 10:03:21 +0530

    Update App.js
    Added user reducer and added logbox for ignore logs

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React,{ useEffect } from "react";
 import AppNavigator from "./navigation/AppNavigator";
+import { LogBox } from "react-native";
 import ReduxThunk from "redux-thunk";
 import { Provider } from "react-redux";
 import { applyMiddleware, combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./store/reducers/auth";
+import users from "./store/reducers/users";
 const rootReducer=combineReducers({
-  auth:authReducer
+  auth:authReducer,
+  user:users,
 });
 const store=configureStore({reducer:rootReducer},applyMiddleware(ReduxThunk))
 export default function App() {
+  useEffect(() => {
+    LogBox.ignoreLogs(["Animated:`useNativeDriver`"]);
+    LogBox.ignoreLogs(["exported from 'deprecated-react-native-prop-types'."]);
+    LogBox.ignoreLogs(["componentWillReceiveProps has been renamed, and is not recommended for use."]);
+  }, [LogBox]);
+  
   return (
     <Provider store={store}>
       <AppNavigator />

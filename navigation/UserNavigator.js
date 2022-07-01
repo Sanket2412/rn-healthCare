67100053b2dd140bcc17d890c982cfaccf10ea41
Sanket Khardekar <khardekarsanket@gmail.com>
2022-07-01 20:19:02 +0530

    Update UserNavigator.js
    Added Activity Indicator

--- navigation/UserNavigator.js
+++ navigation/UserNavigator.js
@@ -1,11 +1,28 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import { View, Text,ActivityIndicator } from "react-native";
 import { useSelector } from "react-redux";
 import DoctorNavigator from "./Navigators/DoctorNavigator";
 import MedicalTestNavigator from "./Navigators/MedicalTestNavigator";
 import PatientNavigator from "./Navigators/PatientNavigator";
 import PharmacyNavigator from "./Navigators/PharmacyNavigator";
 const UserNavigator = (props) => {
-  const userLoggedIn = useSelector((state) => state.user.loggedInUser);
+  const [userLoggedIn,setUserLoggedIn]=useState(null);
+  const [isLoading,setIsLoading]=useState(true);
+  const user = useSelector((state) => state.user.loggedInUser);
+  useEffect(()=>{
+      if(user !== undefined)
+      {
+        setUserLoggedIn(user);
+        setIsLoading(false);
+      }
+  },[user])
+  if (isLoading || userLoggedIn === null) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="lightblue" />
+      </View>
+    );
+  }
   return (
     <Fragment>
       {userLoggedIn.userType === "patient" ? <PatientNavigator /> : null}

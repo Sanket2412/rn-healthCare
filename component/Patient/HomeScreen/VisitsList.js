cdc95511d716f73c6935dae9d62a5276ca0d08cd
Sanket Khardekar <khardekarsanket@gmail.com>
2022-07-01 20:13:35 +0530

    Update VisitsList.js
    Added cancel booking code and dynamic data fetching

--- component/Patient/HomeScreen/VisitsList.js
+++ component/Patient/HomeScreen/VisitsList.js
@@ -1,25 +1,40 @@
-import React,  { useState }  from "react";
-import { Text, View, StyleSheet, FlatList } from "react-native";
-import { Button, Card, List } from "react-native-paper";
-
-const appointmentList = [
-  {
-    id: 1,
-    date: new Date().toString(),
-  },
-  {
-    id: 2,
-    date: "2022-06-01",
-  },
-  {
-    id: 3,
-    date: "2022-05-05",
-  },
-];
-
+import React, { useState } from "react";
+import { Text, View, StyleSheet, FlatList, Alert, TouchableOpacity } from "react-native";
+import { Avatar, Button, Card, List } from "react-native-paper";
+import { useSelector,useDispatch } from "react-redux";
+import * as appointmentActions from "../../../store/actions/appointment";
 const VisitsList = (props) => {
+  const dispatch= useDispatch();
+  const appointmentList = useSelector(
+    (state) => state.appointment.appointments
+  );
+  const user = useSelector((state) => state.user.loggedInUser);
+  let filteredAppointmentList = [];
+  if (appointmentList.length > 0) {
+    filteredAppointmentList = appointmentList.filter((item) => {
+      return item.enabled === true && item.userId === user.key;
+    });
+  }
   let curr = new Date();
-  curr.setHours(0,0,0,0);
+  curr.setHours(0, 0, 0, 0);
+  const onCancelClickHandler=(key)=>{
+    Alert.alert("Are You Sure?","Are You Sure, You Want Cancel Doctor's Appointment",[
+      {
+        text:"Yes",
+        onPress: ()=>{
+          dispatch(appointmentActions.cancelAppointment(key));
+        }
+      },
+      {
+        text:"No",
+      }
+    ])
+  }
+  filteredAppointmentList.sort(function(a,b){
+    var c= new Date(a.bookedDate);
+    var d= new Date(b.bookedDate);
+    return d-c; 
+  })
   return (
     <View style={styles.content}>
       <Card elevation={20} style={{ borderRadius: 15 }}>
@@ -31,39 +46,77 @@ const VisitsList = (props) => {
             alignSelf: "center",
           }}
         />
-        <FlatList
-          data={appointmentList}
-          keyExtractor={(item) => item.id}
-          renderItem={(itemData) => {
-              let title='';
-              let itemDate= new Date(itemData.item.date);
-              if(curr.getDate() === itemDate.getDate() && curr.getMonth() === itemDate.getMonth() && curr.getFullYear() === itemDate.getFullYear() )
-              {
-                  title="Appointment Sceduled Today"
+        {filteredAppointmentList.length === 0 && (
+          <Card.Content>
+            <View
+              style={{
+                height: "60%",
+                width: "100%",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text>Appointments Not Found!</Text>
+            </View>
+          </Card.Content>
+        )}
+        {filteredAppointmentList.length > 0 && (
+          <FlatList
+            data={filteredAppointmentList}
+            keyExtractor={(item) => item.key}
+            renderItem={(itemData) => {
+              let title = "";
+              let isCancellable=false;
+              let itemDate = new Date(itemData.item.bookedDate);
+              if (
+                curr.getDate() === itemDate.getDate() &&
+                curr.getMonth() === itemDate.getMonth() &&
+                curr.getFullYear() === itemDate.getFullYear()
+              ) {
+                title = "Appointment Sceduled Today";
+              } else if (itemDate > curr) {
+                isCancellable=true;
+                title = "Next Appointment Sceduled On";
+              } else {
+                title = "Visited On";
               }
-              else if(itemDate > curr)
-              {
-                title="Next Appointment Sceduled On"
-              }
-              else
-              {
-                  title="Visited On"
-              }
-              return(
-            <View>
-              <Card.Content>
-                <List.Item
-                  key={itemData.item.id}
-                  style={{ height: 80 }}
-                  title={title}
-                  description={new Date(itemData.item.date).toDateString()}
-                />
-              </Card.Content>
-            </View>)
+              return (
+                <View key={itemData.item.key}>
+                  <Card.Content key={itemData.item.key}>
+                    <List.Item
+                      key={itemData.item.key}
+                      style={{ height: 80 }}
+                      title={title}
+                      right={props => isCancellable ? <TouchableOpacity onPress={onCancelClickHandler.bind(this,itemData.item.key)}><Avatar.Icon style={{ backgroundColor: "white" }}
+                      color="red"
+                      size={50}
+                      icon="cancel"/></TouchableOpacity> : null}
+                      description={new Date(
+                        itemData.item.bookedDate
+                      ).toDateString()}
+                    />
+                  </Card.Content>
+                </View>
+              );
+            }}
+          />
+        )}
+        <Card.Actions
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
           }}
-        />
-        <Card.Actions style={{flexDirection:"row", justifyContent:"center",alignItems:"center"}}>
-            <Button icon="calendar" mode="contained" color="green" onPress={props.openDialog}> Book Appointment</Button>
+        >
+          <Button
+            icon="calendar"
+            mode="contained"
+            color="green"
+            onPress={props.openDialog}
+          >
+            Book Appointment
+          </Button>
         </Card.Actions>
       </Card>
     </View>
@@ -73,12 +126,12 @@ const VisitsList = (props) => {
 const styles = StyleSheet.create({
   content: {
     flex: 1,
-    width:"100%",
+    width: "100%",
     padding: 15,
   },
   cardTitle: {
     borderRadius: 15,
-    backgroundColor:"#039dfc",
+    backgroundColor: "#039dfc",
   },
 });
 export default VisitsList;

5fb695df820ef58b15a4ad462c16032d6dce1b14
Sanket Khardekar <khardekarsanket@gmail.com>
2022-07-05 10:55:54 +0530

    Create ViewPatientHistoryScreen.js

---
+++ screens/DoctorScreens/ViewPatientHistoryScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ViewPatientHistoryScreen = (props) => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Patient History Screen</Text>
+    </View>
+  );
+};
+
+export default ViewPatientHistoryScreen;

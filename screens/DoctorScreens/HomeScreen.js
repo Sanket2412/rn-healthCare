870ee17a88ae21e54cf5206d09bdf4882ff12922
Sanket Khardekar <khardekarsanket@gmail.com>
2022-07-13 23:39:02 +0530

    Update HomeScreen.js
    Added Appointment List Component

--- screens/DoctorScreens/HomeScreen.js
+++ screens/DoctorScreens/HomeScreen.js
@@ -1,18 +1,29 @@
-import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
-import { Button } from "react-native-paper";
+import React, { useEffect } from "react";
+import { View ,ImageBackground, StyleSheet, Dimensions } from "react-native";
 import { useDispatch } from "react-redux";
-import * as authActions from "../../store/actions/auth";
+import AppointmentList from "../../component/Doctor/AppoitmentList.js";
+import { background } from "../../constant/constants.js";
+import { fetchAppointments } from "../../store/actions/appointment.js";
+const { height } = Dimensions.get("window");
 const HomeScreen=()=>{
     const dispatch=useDispatch();
-    const onPressLogout=()=>{
-        dispatch(authActions.logout())
-    }
+    useEffect(()=>{
+        dispatch(fetchAppointments());
+    },[dispatch])
     return(
-        <View style={{flex:1, justifyContent:"center",alignItems:"center"}}>
-            <Text>Doctor's HomeScreen</Text>
-            <TouchableOpacity onPress={onPressLogout}><Text>Logout</Text></TouchableOpacity>
-        </View>
+        <ImageBackground source={{uri:background}} style={{flexDirection:"column", height:"100%"}}>
+            <View style={styles.app}>
+                <AppointmentList />
+            </View>
+        </ImageBackground>
     )
 }
+const styles = StyleSheet.create({
+    app: {
+      height: height / 2,
+      paddingTop: height * 0.1,
+      flexDirection: "column",
+      alignItems: "center",
+    },
+  });
 export default HomeScreen;
